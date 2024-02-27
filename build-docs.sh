0142a584d4a47232f8bc6cd6f70c9e6fb8db8e9d
Enrico Daga <enrico.daga@open.ac.uk>
2024-02-27 12:13:22 +0000

    Update

--- build-docs.sh
+++ build-docs.sh
@@ -2,8 +2,9 @@
 
 rm docs/index.html
 lode="http://150.146.207.114"
-url="$lode/lode/extract?url=https%3A%2F%2Fdata.open.ac.uk%2Fontology%2Fnews-classification&lang=en#d4e46"
+url="$lode/lode/extract?url=https%3A%2F%2Fdata.open.ac.uk%2Fontology%2Fnewsclassification&lang=en#d4e46"
 wget --cut-dirs=1  --no-parent --no-host --convert-links -U mozilla --directory-prefix "docs/" --domains "$lode" --no-parent "$url" -O docs/index.html
 
 sed -i ' ' 's/http:\/\/150.146.207.114\///g' docs/index.html
+
 
